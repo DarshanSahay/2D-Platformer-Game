4804e595e9ad1f59656785120ab31a419c95293d
DESKTOP-2CJFRUP\user <darshansahay2808@gmail.com>
2021-07-20 15:04:20 +0530

    Implimented Horizontal Movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    private void Update()
     {
-        
-    }
+        float Speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(Speed));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 scale = transform.localScale;
+        if(Speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(Speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
