ca3384e4f444263b79fe916988a8813c56dc8d06
DESKTOP-2CJFRUP\user <darshansahay2808@gmail.com>
2021-07-21 13:58:30 +0530

    Player Crouch and Jump Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     private void Update()
     {
+        //for movement
+
         float Speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(Speed));
 
@@ -20,5 +22,25 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        //For Jump Implementation
+
+        float Jump = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Jump", Mathf.Abs(Jump));
+
+        if(Jump > 0) 
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", false);
+        }
+
     }
 }
